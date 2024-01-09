c6bd3a11852b0a820a7ac9b76a75eaf351af1412
maullanna <maullanna35@gmail.com>
2024-01-09 20:35:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['index.html', 'login.html'],
+
+    theme: {
+        extend: {
+            colors: {
+                orange: '#F57E2E',
+                hover: '#f1f5f9',
+                background: '#D3D3D3',
+            }
+        },
+
+    },
+    plugins: [],
+}
