0fd36458dc880e2e4c4c0aeb02ed9ea2c26bde46
maullanna <maullanna35@gmail.com>
2024-01-10 13:48:44 +0700

    update image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['index.html', 'login.html'],
+  content: ["login.html"],
 
-    theme: {
-        extend: {
-            colors: {
-                orange: '#F57E2E',
-                hover: '#f1f5f9',
-                background: '#D3D3D3',
-            }
-        },
-
+  theme: {
+    extend: {
+      colors: {
+        orange: "#F57E2E",
+        hover: "#f1f5f9",
+        background: "#D3D3D3",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
